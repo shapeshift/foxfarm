5fca471de7b7598bf685d70b9ca0f04fe1e6d459
andrewpjames13 <andrewpjames87@gmail.com>
2021-09-17 15:03:00 -0600

    useContractMethods can take abi

--- src/hooks/useContractMethods.ts
+++ src/hooks/useContractMethods.ts
@@ -3,9 +3,14 @@ import { useContract } from './useContract'
 import { FOX_ETH_FARMING_ADDRESS } from 'lib/constants'
 import farmAbi from 'abis/farmingAbi.json'
 
-export const useContractMethods = (contractAddress?: string) => {
+export const useContractMethods = (contractAddress?: string, abi?: string) => {
   const provider = useActiveProvider()
-  const contract = useContract(provider, null, contractAddress ?? FOX_ETH_FARMING_ADDRESS, farmAbi)
+  const contract = useContract(
+    provider,
+    null,
+    contractAddress ?? FOX_ETH_FARMING_ADDRESS,
+    abi ?? farmAbi
+  )
 
   return contract
 }
