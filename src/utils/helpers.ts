2e0d1a5417a1029ba857340f5e4b46a1e85a93c4
andrewpjames13 <andrewpjames87@gmail.com>
2021-09-21 13:18:47 -0600

    Add tests for lpUrlFormatter

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -72,7 +72,7 @@ export async function getBufferedGas(
   }
 }
 
-export const lpUrlFormatter = (route: string, lpAddress?: string, stakingAddress?: string) => {
+export const lpUrlFormatter = (route?: string, lpAddress?: string, stakingAddress?: string) => {
   if (!route) {
     if (stakingAddress) return `/fox-farming/liquidity/${lpAddress}/staking/${stakingAddress}`
     return `/fox-farming/liquidity/${lpAddress}`
