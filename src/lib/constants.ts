48a96016b52936ee4e38140781f39333964d5bb0
unclerom <29046942+unclerom@users.noreply.github.com>
2021-10-11 16:27:22 -0400

    leave v2 on for now

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -16,7 +16,7 @@ export const COUNT_DOWN_TIME = 1626447600 * 1000 // July 16 2021 9am MST
 export const FEATURE_FLAGS = {
   airdrop: true,
   ethFoxStakingV1: true,
-  ethFoxStakingV2: false
+  ethFoxStakingV2: true
 }
 
 type TokenProps = {
