0da1f71dd94d5551bcbaa1e607d46fae6ed74442
unclerom <29046942+unclerom@users.noreply.github.com>
2021-10-11 16:11:49 -0400

    update copy for legal

--- src/pages/Farm/APRFluctuates.tsx
+++ src/pages/Farm/APRFluctuates.tsx
@@ -2,10 +2,10 @@ import { Text } from '@chakra-ui/react'
 
 export const APRFluctuates = () => (
   <Text color='gray.500' fontSize='xs' maxW='460px' textAlign='center'>
-    *ShapeShift is rewarding 15,768,000 FOX until October 13, 2021 to liquidity providers that stake
+    *ShapeShift is awarding 15,881,840 FOX until February 24, 2022 to liquidity providers that stake
     their LP tokens. This is an estimated APR calculated based on the current amount of
     FOX-ETH-UNIV2 LP tokens staked in the ShapeShift rewards contract. This APR amount is subject to
     extreme volatility and this amount could drop significantly, especially in the first few weeks
-    after we roll-out FOX yield farming
+    after FOX yield farming v2 starts
   </Text>
 )
