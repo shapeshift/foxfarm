f1b8d2589dbc9f73a116fbb8037de4d52ee7cff5
Adam Samere <asamere@users.noreply.github.com>
2022-07-06 09:27:45 -0700

    Fix v4 apr notice (#100)
    * update apr fluctuation notice for v4
    
    * 11/18, not 11/17 end date for v4

--- src/pages/Farm/APRFluctuates.tsx
+++ src/pages/Farm/APRFluctuates.tsx
@@ -2,7 +2,7 @@ import { Text } from '@chakra-ui/react'
 
 export const APRFluctuates = () => (
   <Text color='gray.500' fontSize='xs' maxW='460px' textAlign='center'>
-    *ShapeShift is awarding 13,500,000 FOX until July 7, 2022 to liquidity providers that stake
+    *ShapeShift is awarding 6,750,000 FOX until November 18, 2022 to liquidity providers that stake
     their LP tokens. This is an estimated APR calculated based on the current amount of
     FOX-ETH-UNIV2 LP tokens staked in the ShapeShift rewards contract. This APR amount is subject to
     extreme volatility and this amount could drop significantly, especially in the first few weeks
